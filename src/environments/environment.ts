91f825c6a01cec9ba05da97b3d18bea60cafb26e
Nguyen Duc Duy <ducduylearn@gmail.com>
2024-08-19 16:56:25 +0700

    Update API URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    API_URL: 'https://api.local.example.com' || 'http://localhost:3000',
+    API_URL: 'https://api.local.example.com' || 'http://localhost:8080',
   };
